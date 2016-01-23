86c6069287b5f112512726df1b3dcb0013198f88
nicolas <nicolas>
2016-01-23 17:24:30 +0100

    comment

--- src/model/AStar.java
+++ src/model/AStar.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 public class AStar<E extends Number> extends Algorithm<E>{
 	
 	public AStar(Environment<E> env){
@@ -8,7 +10,24 @@ public class AStar<E extends Number> extends Algorithm<E>{
 
 	@Override
 	public void grow(int source, int destination) throws UnknownPlace{
+		MaMatrice<Integer> matrice = new MaMatrice<Integer>();
+		ArrayList<Integer> noeudouvert = new ArrayList<Integer>();
+		ArrayList<Integer> noeudferme = new ArrayList<Integer>();
+		noeudferme.add(source);
+		ArrayList<Integer> current_path = new ArrayList<Integer>();
 		
+		ArrayList<Integer> g = new ArrayList<Integer>();
+		ArrayList<Integer> f = new ArrayList<Integer>();
+		
+		while (!noeudouvert.isEmpty()){
+			int min = Integer.MAX_VALUE;
+			
+			for (int i = 0; i < matrice.size(); i++){
+				if (f.get(i) < min && noeudouvert.contains(new Integer(i))){
+					
+				}
+			}
+		}
 	}
 
 	@Override
--- src/model/Dijkstra.java
+++ src/model/Dijkstra.java
@@ -37,10 +37,13 @@ public class Dijkstra<E extends Number> extends Algorithm<E>{
 			try {
 				if ((dist = matrice.get(source, i)).intValue() < Integer.MAX_VALUE){
 					distance.add(dist);
+					//On initialise les noeuds à une distance infini de la source
 				}else{
 					distance.add((E)(Integer)Integer.MAX_VALUE);
+					//On initialise les noeuds adjacents à la source, par leur distance à la source
 				}
 				predecessor.add(source);
+				//On ajoute à chaque noeud comme prédécesseur, eux-mêmes
 			} catch (UnknownPlace e) {
 				System.out.println("Erreur dans le grow de Dijkstra: initialisation");
 				e.printStackTrace();
@@ -49,8 +52,12 @@ public class Dijkstra<E extends Number> extends Algorithm<E>{
 		}
 		
 		while(banlist.size() != matrice.size()){
+			// Tant que tous les noeuds n'ont pas été parcourus
+			
 			int min = Integer.MAX_VALUE;
 			int minnode = destination;
+			
+			//On cherche le noeud n'ont encore parcourus avec la plus petit distance à la source
 			for(int i = 0 ; i < matrice.size() ; i++){
 				if (!banlist.contains(i) && distance.get(i).intValue() < min){
 					min = distance.get(i).intValue();
@@ -58,15 +65,21 @@ public class Dijkstra<E extends Number> extends Algorithm<E>{
 				}
 			}
 			
+			//On indique qu'on l'a parcouru
 			banlist.add(minnode);
 			int newdistance;
 			
 			for (int i = 0; i < matrice.size() ; i++){
 				try {
 					
+					//On fais une mise à jour de la distance à la source pour les noeuds connectés au noeud courant
 					if (!banlist.contains(i) && (newdistance = (Integer) matrice.get(minnode, i)) < Integer.MAX_VALUE){
 						newdistance += distance.get(minnode).intValue(); 
 						if (newdistance < distance.get(i).intValue()){
+							/*Si la distance à la source d'un noeud connecté change,
+							cela signifie que le noeud à parcourir avec ce noeud dans la cadre
+							d'un plus cours chemin est le noeud courant
+							*/
 							distance.set(i, (E) (Integer) newdistance);
 							predecessor.set(i, minnode);
 						}
@@ -82,7 +95,7 @@ public class Dijkstra<E extends Number> extends Algorithm<E>{
 		path.add(destination);
 		int current = destination;
 		
-		
+		//On rétablit le plus court chemin jusqu'à la destination d'après la liste des prédécesseurs
 		while (current != source){
 			int pred = predecessor.get(current);
 			path.add(pred);
