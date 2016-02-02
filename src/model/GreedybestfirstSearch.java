34871a06730c87426b858387d6c51d8d44684264
nicolas <nicolas>
2016-02-02 09:33:36 +0100

    new class

---
+++ src/model/GreedybestfirstSearch.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Observable;
+
+public class GreedybestfirstSearch<E extends Number> extends Observable implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
