998f2b3c4becde3913b111e9556460719aeb8e9a
nicolas <nicolas>
2016-02-02 09:35:36 +0100

    rbfs

---
+++ src/model/RBFS.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Observable;
+
+public class RBFS <E extends Number> extends Observable implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
