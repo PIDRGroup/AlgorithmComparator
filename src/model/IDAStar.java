618bfe417da2387a55112a6040469b65a0e5593a
nicolas <nicolas>
2016-02-29 22:39:31 +0100

    premiere version IDAstar

--- src/model/IDAStar.java
+++ src/model/IDAStar.java
@@ -1,10 +1,24 @@
 package model;
 
+import java.util.ArrayList;
+
 public class IDAStar extends Algorithm{
+	
+	private Environment world;
+	private Place destination;
+	final int found = -1; 
+	
+	public IDAStar(){
+		path = new ArrayList<Place>();
+		eval = new Evaluation(); 
+	}
 
 	@Override
 	public void grow(Environment world, Place src, Place dest)
 			throws UnknownPlaceException {
+		
+		this.world = world;
+		this.destination = dest;
 		
 		if(!world.isPlace(src))
 			throw new UnknownPlaceException(src);
@@ -12,8 +26,49 @@ public class IDAStar extends Algorithm{
 		if(!world.isPlace(dest))
 			throw new UnknownPlaceException(dest);
 		
+		double t;
+		double bound = h(src);
+		
+		while(true){
+			t = search(src, 0, bound);
+			if (t == found) ;
+			if (t == Integer.MAX_VALUE) System.out.println("Aucun chemin trouvé!");
+			bound = t;
+		}
+		
+	}
+	
+	private double search(Place node, double g, double bound) throws UnknownPlaceException{
+		double f = g + h(node);
+		
+		if (f > bound){
+			return f;
+		}
+		
+		if (node.equals(destination)){
+			return found;
+		}
+		
+		double min = Integer.MAX_VALUE;
+		double t;
+		double dist;
+		
+		for (int i = 0; i < world.size(); i++){
+			if ((dist = world.get(node, world.getPlace(i))) < Integer.MAX_VALUE){
+				t = search(world.getPlace(i), g + dist, bound);
+				if (t == found) return found;
+				if (t < min) min = t;
+			}
+		}
+		
+		return min;
 	}
 
+	public double h(Place current){
+		return 0;
+	}
+	
+	
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
