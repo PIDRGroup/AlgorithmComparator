ccadcc142b84a08100eca31b009bc000d4b79d15
Vincent Albert <vincent.albert@telecomnancy.eu>
2016-05-16 11:56:25 +0200

    REPORT

---
+++ tn-report/clean.sh
@@ -0,0 +1,3 @@
+rm *~ *.backup *.aux *.toc *.blg *.log *.out *.bbl *.dvi *.nav *.pyg *.snm *.lof *.lol *.lot 2> /dev/null
+cd files
+rm *~ *.backup *.aux *.toc *.blg *.log *.out *.bbl *.dvi *.nav *.pyg *.snm *.lof *.lol *.lot 2> /dev/null
